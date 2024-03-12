271cd4598708afb316552b73aa8cfe2d18786e7a
Adjei Rapheal <ralphrapheals@gmail.com>
2024-03-12 01:43:47 +0000

    Second commit

--- App.js
+++ App.js
@@ -1,17 +1,76 @@
-import { View} from "react-native";
-
+import { View, SafeAreaView, Text, Image, ScrollView} from "react-native";
+import { AntDesign, Feather } from '@expo/vector-icons';
+import notification from "./src/mock/notificationMock";
 
 const App = () => {
+  console.log("Notification", notification)
   return (
-    <View></View>
+
+    <SafeAreaView className = "mt-10 mx-5 flex-1 space-y-10">
+    <ScrollView  className= "space-y-10 overflow-y-scrollno-scrollbar ">
+    {/**Notification Header */}
+      <View className="flex flex-row items-center justify-between">
+        <View>
+        <AntDesign name="left" size={24} color="black" />
+        </View>
+
+        <View>
+          <Text className="text-lg font-semibold">Notification</Text>
+        </View>
+
+        <View>
+        <Feather name="settings" size={24} color="black" />
+        </View>
+      </View>
+
+
+    {/**Messages */}
+      <View>
+      {/**Message header */}
+          <View className= "flex flex-row justify-between items-center">
+          {/**recent */}
+              <View className = "flex flex-row items-center space-x-1">
+                <Text className =" font-bold text-lg">Recent</Text>
+                <View className="bg-[#ff4747] p-0 rounded-full h-5 w-5 justify-center items-center ">
+                  <Text className= "text-white text-[8px] font-bold">4</Text>
+                </View>  
+              </View>
+          {/**mark as read */}
+              <View>
+                  <Text className= "font-semibold text-[#3f6df6]"> Mark All Read</Text>
+                </View>
+          </View>
+        {/**End of message header */}
+
+
+
+        {/**Message List */}
+        {notification.map((item) => {
+          return(
+            <View key= {item.id}>
+              <View className= "flex flex-row items-center space-x-2 space-y-4 ">
+                <Image source={{uri: item.imageUrl}} className = "w-12 h-12 rounded-full"/>
+                <View className = "flex-1 space-y-1">
+                  <View>
+                  <Text className= "font-semibold">{item.title}</Text>
+                  <Text className = "text-gray-500 max-h-[50px]">{item.message}</Text></View>
+                </View>
+                <View><Text>{item.time}</Text></View>
+              </View>
+              <View className="border-b border-gray-300 mt-2"></View>
+            </View>
+          )
+        }        
+        )}
+        
+      </View>
+
+    </ScrollView>
+    </SafeAreaView>
     
   );
 };
 
 export default App;
 
-{
-  /**<View className = "flex-1 justify-center items-center  bg-black">
-      <Text className = "text-#565556 font-bold text-2xl"> App</Text>
-    </View> */
-}
+
---
+++ src/mock/notificationMock.js
@@ -0,0 +1,86 @@
+const notification = [
+    {
+      id: 1,
+      imageUrl:
+        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Kim Hayo Send You a Message",
+      message:
+        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ..You can check our hote..",
+      time: "1 m ago",
+    },
+    {
+      id: 2,
+      imageUrl:
+        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Kim Hayo Send You a Message",
+      message:
+        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "1 m ago",
+    },
+    {
+      id: 3,
+      imageUrl:
+        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Kim Hayo Send You a Message",
+      message:
+        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "1 m ago",
+    },
+    {
+      id: 4,
+      imageUrl:
+        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Kim Hayo Send You a Message",
+      message:
+        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "1 m ago",
+    },
+    {
+      id: 5,
+      imageUrl:
+        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Kim Hayo Send You a Message",
+      message:
+        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "1 m ago",
+    },
+    {
+        id: 6,
+        imageUrl:
+          "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Kim Hayo Send You a Message",
+        message:
+          "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+        time: "1 m ago",
+      },
+      {
+        id: 7,
+        imageUrl:
+          "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Kim Hayo Send You a Message",
+        message:
+          "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+        time: "1 m ago",
+      },
+      {
+        id: 8,
+        imageUrl:
+          "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Kim Hayo Send You a Message",
+        message:
+          "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+        time: "1 m ago",
+      },
+      {
+        id: 9,
+        imageUrl:
+          "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Kim Hayo Send You a Message",
+        message:
+          "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
+        time: "1 m ago",
+      },
+  ];
+
+
+  export default notification;
